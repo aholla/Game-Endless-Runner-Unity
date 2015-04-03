f21e3e7041cfb75955201079b78b86e0d8c7b7ce
Adam Holland <adgholland@gmail.com>
2015-04-03 09:53:50 +0100

    First!
    Just the external libraries and project skeleton.

---
+++ Assets/Extensions/ExtensionArray.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public static class ExtensionArray {
+
+	/// <summary>
+	/// Suffles the array.
+	/// </summary>
+	public static void Shuffle<T>( this T[] arr ) {		
+		for( int i = 0; i < arr.Length; i++ ) {
+			T tmp = arr[ i ];
+			int r = Random.Range( i, arr.Length );
+			arr[ i ] = arr[ r ];
+			arr[ r ] = tmp;
+		}
+	}
+
+	/// <summary>
+	/// Logs the array items.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="arr">The Array</param>
+	/// <param name="prefix">The prefix.</param>
+	public static void LogArrayItems<T>( this T[] arr, string prefix = "" ) {
+		string outputStr = "" + prefix;
+		for( int i = 0; i < arr.Length; i++ ){
+			outputStr += arr[ i ];
+		}
+		Debug.Log( outputStr );
+	}
+
+
+	
+}
---
+++ Assets/Extensions/ExtensionGameObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public static class ExtensionGameObject {
+
+	public static void EnableChildren( this Component component, System.Func<Component, bool> predicate, bool enabled = true ) {
+		foreach( var c in component.GetComponentsInChildren<Collider>( true ) ) {
+			if( predicate( c ) ) {
+				c.enabled = enabled;
+			}
+		}
+		foreach( var c in component.GetComponentsInChildren<Rigidbody>( true ) ) {
+			if( predicate( c ) ) {
+				c.isKinematic = !enabled;
+			}
+		}
+		foreach( var c in component.GetComponentsInChildren<Behaviour>( true ) ) {
+			if( predicate( c ) ) {
+				c.enabled = enabled;
+			}
+		}
+	}
+
+	public static void EnableChildren( this Component component, bool enabled = true ) {
+		component.EnableChildren( ( a ) => true, enabled );
+	}
+
+	public static void EnableChildren( this GameObject go, bool enabled = true ) {
+		go.transform.EnableChildren( ( a ) => true, enabled );
+	}
+
+	public static void EnableChildren( this GameObject go, System.Func<Component, bool> predicate, bool enabled = true ) {
+		go.transform.EnableChildren( predicate, enabled );
+	}
+
+}
