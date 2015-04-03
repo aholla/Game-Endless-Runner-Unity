cb4761192e72a79c7131ea3862beea35dfd3645f
Adam Holland <adgholland@gmail.com>
2015-04-03 20:45:12 +0100

    New Level chunks
    Created new prefabs that hold obstacles/blades.

--- Assets/_Game/Scripts/LevelGenerator.cs
+++ Assets/_Game/Scripts/LevelGenerator.cs
@@ -7,11 +7,15 @@ public class LevelGenerator : MonoBehaviour {
 	[SerializeField]
 	private CameraController _cameraContoller;
 
-	private ObjectPool _pool;
-	private List<LevelChunk> _levelChunks;
+	[SerializeField]
+	private ObjectPool _objectPoolPlain;
 
+	[SerializeField]
+	private ObjectPool[] _availablePools;
+
+	private List<LevelChunk> _levelChunks;
 	private float _currentPosZ;
-	
+
 	//===================================================
 	// UNITY METHODS
 	//===================================================
@@ -19,8 +23,8 @@ public class LevelGenerator : MonoBehaviour {
 	/// <summary>
 	/// Awake.
 	/// </summary>
-	void Awake () {
-		_pool = GetComponent<ObjectPool>();
+	void Awake() {
+		//_pool = GetComponent<ObjectPool>();
 		_currentPosZ = 0.0f;
 		_levelChunks = new List<LevelChunk>();
 	}
@@ -28,17 +32,20 @@ public class LevelGenerator : MonoBehaviour {
 	/// <summary>
 	/// Start.
 	/// </summary>
-	void Start () {
-		_pool.Init();
+	void Start() {
+		for( int i = 0; i < _availablePools.Length; i++ ) {
+			ObjectPool pool = _availablePools[ i ];
+			pool.Init();
+		}
 
 		// create initial chunks.
 		CreateFirstSetOfChunks();
 	}
-	
+
 	/// <summary>
 	/// Update.
 	/// </summary>
-	void Update () {
+	void Update() {
 
 		// if camera position is greater than the current level size, spawn a new level chunk.
 		if( _currentPosZ <= _cameraContoller.Far ) {
@@ -48,7 +55,7 @@ public class LevelGenerator : MonoBehaviour {
 		// remove the old chunks.
 		CullOldChunks();
 	}
-	
+
 
 	//===================================================
 	// PUBLIC METHODS
@@ -59,29 +66,34 @@ public class LevelGenerator : MonoBehaviour {
 	//===================================================
 	// PRIVATE METHODS
 	//===================================================
-	
+
 	/// <summary>
 	/// Creates the first set of chunks.
 	/// </summary>
 	private void CreateFirstSetOfChunks() {
 		// while within range of the camera, add more level pieces.
-		for( int i = 0; i < 1; i++ ) {
-			//SpawnLevelChunk();
-		}
-
 		while( _currentPosZ < _cameraContoller.Far ) {
-			SpawnLevelChunk();
+			SpawnLevelChunk( true );
 		}
 	}
 
 	/// <summary>
 	/// Spawns the level chunk from the object pool
 	/// </summary>
-	private void SpawnLevelChunk() {
+	private void SpawnLevelChunk( bool forcePlainChunk = false ) {
+		// select a chunk from a random pool if not forced plain. There are more plain chunks than obstacle chunks.
+		ObjectPool selectedPool;
+		if( !forcePlainChunk ) {
+			int random = Random.Range( 0, _availablePools.Length );
+			selectedPool = _availablePools[ random ];
+		} else {
+			selectedPool = _objectPoolPlain;
+		}
+
 		// get gameobject from pool.
-		GameObject levelGO = _pool.GetGameObject();
+		GameObject levelGO = selectedPool.GetGameObject();
 		levelGO.transform.position = new Vector3( transform.position.x, transform.position.y, _currentPosZ );
-		
+
 		// increment the z position.
 		LevelChunk chunk = levelGO.GetComponent<LevelChunk>();
 		chunk.Init();
@@ -94,7 +106,7 @@ public class LevelGenerator : MonoBehaviour {
 	/// Culls the old chunks when they are behind the camera.
 	/// </summary>
 	private void CullOldChunks() {
-		for( int i = _levelChunks.Count-1; i >=0; i-= 1 ) {
+		for( int i = _levelChunks.Count - 1; i >= 0; i -= 1 ) {
 			LevelChunk chunk = _levelChunks[ i ];
 
 			if( chunk.FarEdge < _cameraContoller.PosZ ) {
