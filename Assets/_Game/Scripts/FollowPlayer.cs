09c2a56f4307892fc624e8fc5ee9c5bd240e5f07
Adam Holland <adgholland@gmail.com>
2015-04-03 13:27:58 +0100

    Player moves and camera follows.
    Simple camera follow script and player movement script.

---
+++ Assets/_Game/Scripts/FollowPlayer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	[SerializeField]
+	private Transform _target;
+
+	[SerializeField]
+	private float _ease;
+
+	private Vector3 _startPosition;
+	private Vector3 _offset;
+
+	//===================================================
+	// UNITY METHODS
+	//===================================================
+
+	/// <summary>
+	/// Awake.
+	/// </summary>
+	void Awake () {
+		
+	}
+
+	/// <summary>
+	/// Start.
+	/// </summary>
+	void Start () {
+		_startPosition = transform.position;
+		_offset = _target.position - transform.position;
+	}
+	
+	/// <summary>
+	/// Update.
+	/// </summary>
+	void Update () {
+		transform.position = new Vector3( _startPosition.x, _startPosition.y, _target.position.z - _offset.z );
+	}
+
+	//===================================================
+	// PUBLIC METHODS
+	//===================================================
+
+
+
+	//===================================================
+	// PRIVATE METHODS
+	//===================================================
+
+
+
+	//===================================================
+	// EVENTS METHODS
+	//===================================================
+
+
+
+}
---
+++ Assets/_Game/Scripts/PlayerMove.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	[SerializeField]
+	private float _acceleration;
+
+	[SerializeField]
+	private float _maxSpeed;
+
+	private float _speed;
+	private bool _isRunning;	
+	
+	//===================================================
+	// UNITY METHODS
+	//===================================================
+
+	/// <summary>
+	/// Awake.
+	/// </summary>
+	void Awake () {
+		
+	}
+
+	/// <summary>
+	/// Start.
+	/// </summary>
+	void Start () {
+		_isRunning = true;
+		_speed = 0.0f;
+	}
+	
+	/// <summary>
+	/// Update.
+	/// </summary>
+	void Update () {
+		if( _isRunning ) {
+			_speed += _acceleration;
+			if( _speed > _maxSpeed ) {
+				_speed = _maxSpeed;
+			}
+			transform.Translate( new Vector3( 0, 0, _speed ) );
+		}
+	}
+
+	//===================================================
+	// PUBLIC METHODS
+	//===================================================
+
+
+
+	//===================================================
+	// PRIVATE METHODS
+	//===================================================
+
+
+
+	//===================================================
+	// EVENTS METHODS
+	//===================================================
+
+
+
+}
